5cd94a7ea2103fc1ff4b61633a861d78e7341aee
smykovvd <145441418+smykovvd@users.noreply.github.com>
2025-10-30 22:11:44 +0300

    Little config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/web_practic2/',
+    base: './',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
